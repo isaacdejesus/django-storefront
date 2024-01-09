ffe8cf2d6bd5a81cab23245314f1c34d12673b79
isaacdejesus <isaacderxc@gmail.com>
2024-01-08 23:44:40 -0600

    Installed and configured mysql and mysqlworkbench. Worked on migrations and created fake data.

--- store/models.py
+++ store/models.py
@@ -9,8 +9,9 @@ class Collection(models.Model):
     
 class Product(models.Model):
     title = models.CharField(max_length=255)
+    slug = models.SlugField()
     description = models.TextField()
-    price = models.DecimalField(max_digits=6, decimal_places=2)
+    unit_price = models.DecimalField(max_digits=6, decimal_places=2)
     inventory = models.IntegerField()
     last_update = models.DateTimeField(auto_now=True)
     collection = models.ForeignKey(Collection, on_delete=models.PROTECT)
@@ -55,6 +56,7 @@ class OrderItem(models.Model):
 class Address(models.Model):
     street = models.CharField(max_length=255)
     city = models.CharField(max_length=255)
+    zip = models.CharField(max_length=5, default='00000')
     customer = models.OneToOneField(Customer, on_delete=models.CASCADE, primary_key=True)
 
 class Cart(models.Model):
--- store_front/settings.py
+++ store_front/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'debug_toolbar',
     'store',
     'tags',
+    'likes',
 ]
 
 MIDDLEWARE = [
@@ -85,8 +86,11 @@ WSGI_APPLICATION = 'store_front.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'storefront',
+        'HOST': 'localhost',
+        'USER': 'root',
+        'PASSWORD': 'gtK-meH8$'
     }
 }
 
